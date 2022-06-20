4469958b4aee492d0e36b5d42aa1a608e9e41afd
IshaanKannan1 <ikannan@caltech.edu>
2022-06-19 23:27:19 -0700

    Untested f(z) and tested SD averaging

---
+++ MATLAB/iteration/comp_f_iterfun.m
@@ -0,0 +1,77 @@
+function prod = comp_f_iterfun(n, z, params, p)
+%     comp_f_iterfun efficiently computes each term in the iteration that 
+%     factorizes over vertices, making use of PauliRotations to avoid
+%     repeated exponentiation. 
+%     
+%     Usage: out = comp_f_iterfun(randn, bitstring, params, p)
+%     
+%     Arguments:
+%         n = a 3x1 vector on the unit sphere
+%         z = a length 2p+2 bitstring representing our spin state basis
+%         params = a px3 matrix specifying parameters beta, gamma, delta
+%                  in that order, for each depth from 1 to p
+%         p = QAOA depth 
+
+% compute product of exponential terms
+    prod = 1;
+    for i = 1:2*p+1
+        R = z(i+1);
+        L = z(i);
+        beta_i = params(i, 1);
+        gamma_i = params(i, 2);
+        delta_i = params(i, 3);
+
+        if i <= p
+            if R == 1 && L == 1
+                prod = prod * equal_z(beta_i, gamma_i, delta_i, n, 1);
+            elseif R == -1 && L == -1
+                prod = prod * equal_z(beta_i,gamma_i, delta_i, n, 0);
+            elseif R == -1 && L == 1
+                prod = prod * z1_pos_z2_neg(beta_i, gamma_i, delta_i, n);
+            else
+                prod = prod * z1_neg_z2_pos(beta_i, gamma_i, delta_i, n);
+            end
+        else
+            if R == 1 && L == 1
+                prod = prod * equal_z(beta_i, gamma_i, delta_i, n, 0);
+            elseif R == -1 && L == -1
+                prod = prod * equal_z(beta_i, gamma_i, delta_i, n, 1);
+            elseif R == -1 && L == 1
+                prod = prod * z1_pos_z2_neg_p(beta_i, gamma_i, delta_i, n);
+            else
+                prod = prod * z1_neg_z2_pos_p(beta_i, gamma_i, delta_i, n);
+            end   
+        end
+    end
+    prod = prod * 1/4*(1+n(1)-1i*n(1)*z(2*p+2)+n(3)*z(2*p+2) + z(1)* ...
+           (1i*n(2) + n(3) + z(2*p+2) - n(1)*z(2*p+2)));
+end
+
+function v = equal_z(b, g, d, n, pos)
+    a = sqrt(b^2+2*b*d*n(1)+d^2*n(1)^2 + (g+d*n(2))^2 +d^2*n(3)^2);
+    if pos == 1
+        v = cos(a) + 1i*d*n(3)*sin(a)/a; 
+    else
+        v = cos(a) - 1i*d*n(3)*sin(a)/a;
+    end
+end
+
+function v = z1_pos_z2_neg(b, g, d, n)
+    a = sqrt(b^2+2*b*d*n(1)+d^2*n(1)^2 + (g+d*n(2))^2 +d^2*n(3)^2);
+    v = (1i*b + g+ d*(1i*n(1) + n2)) * sin(a)/a;
+end
+
+function v = z1_neg_z2_pos(b, g, d, n)
+    a = sqrt(b^2+2*b*d*n(1)+d^2*n(1)^2 + (g+d*n(2))^2 +d^2*n(3)^2);
+    v = 1i*(b + 1i*g + d*(n(1) + 1i*n2)) * sin(a)/a;
+end
+
+function v = z1_pos_z2_neg_p(b, g, d, n)
+    a = sqrt(b^2+2*b*d*n(1)+d^2*n(1)^2 + (g+d*n(2))^2 +d^2*n(3)^2);
+    v = -1i*(b - 1i*(g + d*(1i*n(1) + n2))) * sin(a)/a;
+end
+
+function v = z1_neg_z2_pos_p(b, g, d, n)
+    a = sqrt(b^2+2*b*d*n(1)+d^2*n(1)^2 + (g+d*n(2))^2 +d^2*n(3)^2);
+    v = (-1i*b + g + d*(-1i*n(1) + n2)) * sin(a)/a;
+end

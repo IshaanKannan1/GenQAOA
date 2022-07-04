77d949db545fa4af35da9b0359c0145ecc275a29
IshaanKannan1 <81390929+IshaanKannan1@users.noreply.github.com>
2022-07-04 15:47:06 -0700

    Update test_iteration.m

--- MATLAB/iteration/test_iteration.m
+++ MATLAB/iteration/test_iteration.m
@@ -21,21 +21,18 @@ end
 J = [wg, -ones(size(wg,1), 1)];
 
 [QAOAhelperfcn, HamObj, HamC, HamZ, HamB, HamD, EvolC, EvolZ, EvolB, EvolD] = SetupQMCHams(n, J, ones(n,1), psi, v, 1);
-param0 = 2*rand(p,4)-1;
-myfun = @(param) QAOAhelperfcn(p, param);
-options = optimoptions('fminunc','GradObj','on','Hessian','off','Display','off',...
-    'TolX',1e-5,'TolFun',1e-5, 'Algorithm', 'quasi-newton','PlotFcns',{@optimplotfval, @optimplotstepsize},...
-    'MaxFunEvals', Inf, 'MaxIter', Inf);
-[x, fval] = fminunc(myfun, param0, options);
-iter_val = 1/2 * (1 - calc_exp(p, x, D, 2, 2) - ...
-            calc_exp(p, x, D, 3, 3) - calc_exp(p, x, D, 4, 4));
--fval
+param0 = ones(p,4);
+% myfun = @(param) QAOAhelperfcn(p, param);
+% options = optimoptions('fminunc','GradObj','on','Hessian','off','Display','off',...
+%     'TolX',1e-5,'TolFun',1e-5, 'Algorithm', 'quasi-newton','PlotFcns',{@optimplotfval, @optimplotstepsize},...
+%     'MaxFunEvals', Inf, 'MaxIter', Inf);
+% [x, fval] = fminunc(myfun, param0, options);
+[F, Fgrad, psi] = QAOAhelperfcn(p, param0);
+iter_val = 1/2 * (1 - calc_exp(p, param0, D, 2, 2) - ...
+            calc_exp(p, param0, D, 3, 3) - calc_exp(p, param0, D, 4, 4));
+-F
 iter_val
 % z = [1 1 1 1 1 1 1 1];
 % zm = -z;
 % avg_f(z, x(:,2:end), p)
 % avg_f(zm, x(:,2:end), p)
-
-
-
-

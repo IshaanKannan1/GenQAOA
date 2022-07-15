acca3cd13be4444274c96e4ac51d35ec69044ba1
Leo Zhou <leologist@gmail.com>
2022-07-15 16:17:30 -0500

    Fixed issue with prod factors in comp_f_iterfun

--- MATLAB/iteration/comp_f_iterfun.m
+++ MATLAB/iteration/comp_f_iterfun.m
@@ -31,67 +31,36 @@ function prod = comp_f_iterfun(n, z, params, p)
         .* cross(m.', repmat(n, p, 1).').' ) ./sin_t;
 
     prod = ones(size(z, 1), 1);
+
     j = 1;
     while j <= 2*p+1
-        R = z(:, j+1);
-        L = z(:, j);
         if j <= p
-            prod = prod .* (cos_t(j) + 1i * sin_t(j) * r(j, 3) * (R+1).*(L+1)/4 ...
-                          + cos_t(j) - 1i * sin_t(j) * r(j, 3)* (1-R).*(1-L)/4 ...
-                          + 1i * (r(j, 1) + 1i * r(j, 2)) * sin_t(j) * (L+1).*(1-R)/4 ...
-                          + 1i * (r(j, 1) - 1i * r(j, 2)) * sin_t(j) * (1-L).*(R+1)/4);
+            R = z(:, j+1);
+            L = z(:, j);
+            prod = prod .* ( (cos_t(j) + 1i * sin_t(j) * r(j, 3)) * (1+L).*(1+R)/4 ...
+                           + (cos_t(j) - 1i * sin_t(j) * r(j, 3)) * (1-L).*(1-R)/4 ...
+                           + (1i * (r(j, 1) + 1i * r(j, 2)) * sin_t(j)) * (1+L).*(1-R)/4 ...
+                           + (1i * (r(j, 1) - 1i * r(j, 2)) * sin_t(j)) * (1-L).*(1+R)/4);
         else
             k = 2*p+2 - j;
-            prod = prod .* (cos_t(k) - 1i * sin_t(k) * r(k, 3) * (R+1).*(L+1)/4 ...
-                          + cos_t(k) + 1i * sin_t(k) * r(k, 3)* (1-R).*(1-L)/4 ...
-                          - 1i * (-r(k, 1) +1i * r(k, 2)) * sin_t(k) * (L+1).*(1-R)/4 ...
-                          - 1i * (-r(k, 1) -1i * r(k, 2)) * sin_t(k) * (1-L).*(R+1)/4);
+            R = z(:, j);
+            L = z(:, j+1);
+            
+            prod = prod .* ...
+                        conj((cos_t(k) + 1i * sin_t(k) * r(k, 3)) * (1+L).*(1+R)/4 ...
+                           + (cos_t(k) - 1i * sin_t(k) * r(k, 3)) * (1-L).*(1-R)/4 ...
+                           + (1i * (r(k, 1) + 1i * r(k, 2)) * sin_t(k)) * (1+L).*(1-R)/4 ...
+                           + (1i * (r(k, 1) - 1i * r(k, 2)) * sin_t(k)) * (1-L).*(1+R)/4);
         end
-% 
-%         if j <= p
-%             beta_i = params(j, 1);
-%             gamma_i = params(j, 2);
-%             delta_i = params(j, 3);
-%             prod = prod .* (equal_z(beta_i, gamma_i, delta_i, n, 1) * (R+1).*(L+1)/4 ...
-%                          + equal_z(beta_i,gamma_i, delta_i, n, 0) * (1-R).*(1-L)/4 ...
-%                          + z1_pos_z2_neg(beta_i, gamma_i, delta_i, n) * (L+1).*(1-R)/4 ...
-%                          + z1_neg_z2_pos(beta_i, gamma_i, delta_i, n) * (1-L).*(R+1)/4);
-%         else
-%             beta_i = params(2*p+2 - j,1);
-%             gamma_i = params(2*p+2 - j, 2);
-%             delta_i = params(2*p+2 - j, 3);
-%             prod = prod .* (equal_z(-beta_i, -gamma_i, -delta_i, n, 1) * (R+1).*(L+1)/4 ...
-%                          + equal_z(-beta_i, -gamma_i, -delta_i, n, 0) * (1-R).*(1-L)/4 ...
-%                          + z1_pos_z2_neg(-beta_i, -gamma_i, -delta_i, n) * (L+1).*(1-R)/4 ...
-%                          + z1_neg_z2_pos(-beta_i, -gamma_i, -delta_i, n) * (1-L).*(R+1)/4);
-%         end
+    
         if j == p
             j = j + 2;
         else
             j = j + 1;
         end
     end
+
     z1 = z(:, 1);
     zp = z(:, 2*p+2);
     prod = prod .* 1/4.*(1 + n(1) + n(3).*z1 + 1i*n(2).*(z1-zp) - n(1).*zp.*z1 + (n(3) + z1).*zp);
 end
-
-% 
-% function v = equal_z(b, g, d, n, pos)
-%     a = sqrt(b^2 + 2*b*d*n(1) + d^2*(n(1)^2 + n(2)^2) + (g + d*n(3))^2);
-%     if pos == 1
-%         v = exp(-1i*a) * (exp(2*1i*a)*(g + d*n(3) + a) - g - d*n(3) + a) / (2*a); 
-%     else
-%         v = exp(-1i*a) * (-exp(2*1i*a)*(g + d*n(3) - a) + g + d*n(3) + a) / (2*a);
-%     end
-% end
-% 
-% function v = z1_pos_z2_neg(b, g, d, n)
-%     a = sqrt(b^2 + 2*b*d*n(1) + d^2*(n(1)^2 + n(2)^2) + (g + d*n(3))^2);
-%     v = (1i*(b + d*n(1)) + d*n(2)) * sin(a)/a;
-% end
-% 
-% function v = z1_neg_z2_pos(b, g, d, n)
-%     a = sqrt(b^2+2*b*d*n(1)+d^2*n(1)^2 + (g+d*n(2))^2 +d^2*n(3)^2);
-%     v = 1i*(b + d*(n(1) + 1i*n(2))) * sin(a)/a;
-% end

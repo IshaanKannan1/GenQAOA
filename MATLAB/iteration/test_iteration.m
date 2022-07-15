712ec2e273885ffda2eb2a99056632bf02a485bf
Leo Zhou <leologist@gmail.com>
2022-07-15 16:45:37 -0500

    Fixed initial state in test_iteration.m

--- MATLAB/iteration/test_iteration.m
+++ MATLAB/iteration/test_iteration.m
@@ -1,8 +1,10 @@
 % set up graph and random vectors
-p = 2;
-D = 2;
-k = 2;
-n = 2*(2^(k+1) - 1);
+
+p = 2; % QAOA depth
+D = 2; % graph degree
+k = 2; % depth of tree
+n = 2*(2^(k+1) - 1); % number of vertices
+
 [wg, A] = gen_bbst(k + 1);
 v = normrnd(0, 1, [n,3]);
 v = normr(v);
@@ -10,29 +12,30 @@ v = normr(v);
 % set up initial state
 up = [1;0];
 down = [0;1];
-theta = pi - acos(v(:,3));
-phi = -atan2(v(:,2), v(:,1));  
-psi = cos(theta(1)/2).*down + (cos(phi(1)) + 1i*sin(phi(1)))*sin(theta(1)/2).*up;
+theta = acos(v(:,3));
+phi = atan2(v(:,2), v(:,1));  
+
+psi = cos(theta(1)/2)*up + exp(1i*phi(1)) * sin(theta(1)/2)*down;
 for j = 2:n
-    q = cos(theta(j)/2).*down + (cos(phi(j)) + 1i*sin(phi(j)))*sin(theta(j)/2).*up;
+    q = cos(theta(j)/2)*up + exp(1i*phi(j)) * sin(theta(j)/2)*down;
     psi = kron(psi, q);
 end
 
+%%
+
 J = [wg, -ones(size(wg,1), 1)];
 
-[QAOAhelperfcn, HamObj, HamC, HamZ, HamB, HamD, EvolC, EvolZ, EvolB, EvolD] = SetupQMCHams(n, J, ones(n,1), psi, v, 1);
+[QAOAhelperfcn, HamObj, HamC, HamZ, HamB, HamD, EvolC, EvolZ, EvolB, EvolD] = ...
+    SetupQMCHams(n, J, ones(n,1), psi, v, 1);
 param0 = ones(p,4);
+
 % myfun = @(param) QAOAhelperfcn(p, param);
 % options = optimoptions('fminunc','GradObj','on','Hessian','off','Display','off',...
 %     'TolX',1e-5,'TolFun',1e-5, 'Algorithm', 'quasi-newton','PlotFcns',{@optimplotfval, @optimplotstepsize},...
 %     'MaxFunEvals', Inf, 'MaxIter', Inf);
 % [x, fval] = fminunc(myfun, param0, options);
-[F, Fgrad, psi] = QAOAhelperfcn(p, param0);
+[F, Fgrad, psiout] = QAOAhelperfcn(p, param0);
 iter_val = 1/2 * (1 - calc_exp(p, param0, D, 2, 2) - ...
             calc_exp(p, param0, D, 3, 3) - calc_exp(p, param0, D, 4, 4));
 -F
 iter_val
-% z = [1 1 1 1 1 1 1 1];
-% zm = -z;
-% avg_f(z, x(:,2:end), p)
-% avg_f(zm, x(:,2:end), p)

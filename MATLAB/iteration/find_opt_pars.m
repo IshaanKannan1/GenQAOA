cc65966bafbabe2c2fb2275b3418b492a5096560
IshaanKannan1 <81390929+IshaanKannan1@users.noreply.github.com>
2022-07-21 13:05:40 -0700

    Update find_opt_pars.m

--- MATLAB/iteration/find_opt_pars.m
+++ MATLAB/iteration/find_opt_pars.m
@@ -1,20 +1,24 @@
-function [best_v, best_pars] = find_opt_pars(p, num_p)
-% Test D = 1, 2, 3, 100  
-    fname = strcat("iteration/paramsearch/p:", num2str(p), ".mat");
-    D = [1, 2, 3, 100];
-    data = cell(length(D), num_p);
-    best_v = 10000;
-    best_pars = ones(1, 4);
+function [best_v, best_pars, best_D] = find_opt_pars(p, num_p, D)
+    fname = join([fileparts(mfilename('fullpath')) strcat("/paramsearch/p:", num2str(p), ".mat")], "");
+    % D = [1, 2, 3, 10, 100];
+    data(num_p*length(D)) = struct();
+    ctr = 1;
     for i = 1:length(D)
         for j = 1:num_p
-            param0 = (2*rand(p, 4) - 1) .* pi/2;
+            disp([D(i) j]);
+            param0 = (2*rand(p, 4)-1) .* pi/2;
             [x, fval, exitflag] = opt_params(p, param0, D(i));
-            data{i, j} = {param0, x, exitflag, fval};
-            if fval < best_v
-                best_v = fval;
-                best_pars = x;                
-            end
+            data(ctr).D = D(i);
+            data(ctr).energy = fval;
+            data(ctr).exitflag = exitflag;
+            data(ctr).initpars = param0;
+            data(ctr).finalpars = mod(x, 2*pi);
+            ctr = ctr + 1;
         end
     end
     save(fname, "data");
+    [best_v, I] = min([data.energy]);
+    best_v = -best_v;
+    best_pars = data(I).finalpars;
+    best_D = data(I).D;
 end

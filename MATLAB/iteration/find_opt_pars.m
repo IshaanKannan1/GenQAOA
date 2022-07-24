7c3f78ed86a1626d62a5081a3b249a74b31e1480
Leo Zhou <leologist@gmail.com>
2022-07-23 22:28:32 -0700

    Tweaks to functions for optimization runs

--- MATLAB/iteration/find_opt_pars.m
+++ MATLAB/iteration/find_opt_pars.m
@@ -1,32 +1,50 @@
-function [best_v, best_pars, best_D] = find_opt_pars(p, num_p, D)
+function data = find_opt_pars(p, nTrials, Ds, runID)
 % Uses gradient descent on num_p randomly sampled points from a 4p-dimensional cube of side length
 % pi from -pi/2 to pi/2 to compute optimal parameters for QMC QAOA iteration for given degrees.
 % Saves resulting data in a struct file.
 % Arguments:
 %   - p: QAOA depth
-%   - num_p: Desired number of initial points
+%   - nTrials: Desired number of initial points
 %   - D: List of positive integers indicating degrees for which we want to optimize
+%
+% Returns:
+%   best_v, best_pars, best_D
 
-    fname = join([fileparts(mfilename('fullpath')) strcat("/paramsearch/p:", num2str(p), ".mat")], "");
-    % D = [1, 2, 3, 10, 100];
-    data(num_p*length(D)) = struct();
-    ctr = 1;
-    for i = 1:length(D)
-        for j = 1:num_p
-            disp([D(i) j]);
+    fname = [fileparts(mfilename('fullpath')), ...
+             sprintf('/paramsearch/p=%d_ID=%d.mat',p,runID)];
+%     Ds = [1, 2, 3, 5, 10, 20, 100];
+
+    rng(20220723+997*runID+p);
+
+    fprintf('Saving to %s\n', fname);
+
+    data(length(Ds), nTrials).D = [];
+    data(length(Ds), nTrials).energy = [];
+    data(length(Ds), nTrials).exitflag = [];
+    data(length(Ds), nTrials).initpars = [];
+    data(length(Ds), nTrials).finalpars = [];
+    data(length(Ds), nTrials).funcCount = [];
+    data(length(Ds), nTrials).realTime = [];
+
+    for i = 1:length(Ds)
+        for j = 1:nTrials
+            tic;
+%             disp([i,j])
             param0 = (2*rand(p, 4)-1) .* pi/2;
-            [x, fval, exitflag] = opt_params(p, param0, D(i));
-            data(ctr).D = D(i);
-            data(ctr).energy = fval;
-            data(ctr).exitflag = exitflag;
-            data(ctr).initpars = param0;
-            data(ctr).finalpars = mod(x, 2*pi);
-            ctr = ctr + 1;
+            [x, fval, exitflag, funcCount] = opt_params(p, param0, Ds(i));
+            data(i, j).D = Ds(i);
+            data(i, j).energy = fval;
+            data(i, j).exitflag = exitflag;
+            data(i, j).initpars = param0;
+            data(i, j).finalpars = x;
+            data(i, j).funcCount = funcCount;
+            data(i, j).realTime = toc;
+            fprintf('p=%d, D=%d, trial=%d, took %0.2f s\n', p, Ds(i), j, toc);
         end
     end
-    save(fname, "data");
-    [best_v, I] = min([data.energy]);
-    best_v = -best_v;
-    best_pars = data(I).finalpars;
-    best_D = data(I).D;
+    save(fname, 'data');
+%     [best_v, I] = min([data.energy], [], 2);
+%     best_v = -best_v;
+%     best_pars = data(I).finalpars;
+%     best_D = data(I).D;
 end
--- MATLAB/iteration/opt_params.m
+++ MATLAB/iteration/opt_params.m
@@ -1,8 +1,10 @@
-function [x, fval, exitflag] = opt_params(p, params, D)  
-    optfun = @(param0) -real(1/2 * (1 - calc_exp(p, param0, D, 2, 2) - ...
-                             calc_exp(p, param0, D, 3, 3) - calc_exp(p, param0, D, 4, 4)));
+function [x, fval, exitflag, funcCount] = opt_params(p, params, D)  
+%     optfun = @(param0) -real(1/2 * (1 - calc_exp(p, param0, D, 2, 2) - ...
+%                              calc_exp(p, param0, D, 3, 3) - calc_exp(p, param0, D, 4, 4)));
+    optfun = @(param) calc_Heisenberg_exp(p, param, D);
     options = optimoptions('fminunc','GradObj','off','Hessian','off','Display','off',...
         'TolX',1e-5,'TolFun',1e-5, 'Algorithm', 'quasi-newton',...
         'MaxFunEvals', Inf, 'MaxIter', Inf);
-    [x, fval, exitflag] = fminunc(optfun, params, options);
+    [x, fval, exitflag, output] = fminunc(optfun, params, options);
+    funcCount = output.funcCount;
 end
